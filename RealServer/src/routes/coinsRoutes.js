d6cf88dfe6b1390aeb88514d7fd5aeb40526ca95
Pablo <pablo_hortelano@hotmail.comm>
2020-09-10 05:30:44 +0200

    getCoinDetails define in another place, GET route fixed

--- RealServer/src/routes/coinsRoutes.js
+++ RealServer/src/routes/coinsRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const getDB = require("../config/db");
 const movements = require("../../public/mocks/db.json");
+const getCoinDetails = require("../../public/api/getCoinDetails");
 
 const dbRouter = express.Router();
 
@@ -9,48 +10,12 @@ function router() {
   dbRouter.route("/").get((req, res) => {
     (async () => {
       const db = await getDB();
-      /* await db.collection(collectionName).deleteMany({}); */
       const collection = await db.collection(collectionName);
       const response = await collection.find().toArray();
       console.log(response);
-      /* .insertMany(movements); */
-
       res.json(response);
     })();
   });
-  function getCoinDetails(coin, userID) {
-    switch (coin) {
-      case "Bitcoin":
-        return {
-          userID,
-          name: coin,
-          description: "BTC",
-          moves: [],
-          img:
-            "https://img2.freepng.es/20171220/bhe/bitcoin-png-5a3a2702388611.73714972151376051423153857.jpg",
-        };
-      case "Ethereum":
-        return {
-          userID,
-          name: coin,
-          description: "ETH",
-          moves: [],
-          img:
-            "https://i0.pngocean.com/files/906/776/980/ethereum-blockchain-bitcoin-logo-see-you-there.jpg",
-        };
-      case "Litecoin":
-        return {
-          userID,
-          name: coin,
-          description: "LTC",
-          moves: [],
-          img:
-            "https://w7.pngwing.com/pngs/777/875/png-transparent-bitcoin-computer-icons-cryptocurrency-litecoin-bitcoin-text-trademark-logo.png",
-        };
-      default:
-        throw new Error("Coin details lookup failed");
-    }
-  }
 
   dbRouter.route("/").post((req, res) => {
     (async () => {
