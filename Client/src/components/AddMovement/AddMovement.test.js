ff66dc4b0367c20b8e4bd3872571392bea1e44ec
Pablo <pablo_hortelano@hotmail.comm>
2020-09-17 00:25:40 +0200

    Add Movement testing

--- Client/src/components/AddMovement/AddMovement.test.js
+++ Client/src/components/AddMovement/AddMovement.test.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
-import { AddMovement } from './AddMovement';
+import AddMovement  from './AddMovement';
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router } from "react-router-dom";
-
-
-//  para que en todos los tests devuelva la misma respuesta
-// jest.mock("@auth0/auth0-react", () => ({
-//   useAuth0: () => ({user : null, isAuthenticated: null, isLoading: null})
-// }));
+import configureStore from 'redux-mock-store';
+import { Provider as ReduxProvider } from "react-redux";
+import thunk from "redux-thunk";
 
 
 jest.mock('@auth0/auth0-react');
 jest.spyOn(console, 'error').mockImplementation(() => { });
 
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares)
+const store = mockStore({
+  movements: [],
+  newCoins: [],
+  moves: [],
+  userCoins: []
+});
+
 
 test('Should redirect to login when user is not Authenticated', () => {
   useAuth0.mockReturnValue({ user: null, isAuthenticated: false, isLoading: false})
-  const { asFragment } = render(<AddMovement userCoins={[]} />)
+  const { asFragment } = render(
+    <ReduxProvider store={store}>
+      <AddMovement userCoins={[]} />
+    </ReduxProvider>
+  )
+
   expect(asFragment()).toMatchSnapshot()
   expect(screen.getByText('Log In')).toBeInTheDocument(); // existencia
   expect(screen.queryByText('Movement details')).toBeNull() // no existencia
@@ -26,26 +37,27 @@ test('Should redirect to login when user is not Authenticated', () => {
 
 test('AddMovement renders correctly', () => {
   useAuth0.mockReturnValue({ user: {email: 'foo@bar.com' }, isAuthenticated: true, isLoading: false})
-  const { asFragment} = render(
-    <Router>
-      <AddMovement userCoins={[]} dispatch={() => { }} />
-    </Router>
+  const { asFragment } = render(
+    <ReduxProvider store={store}>
+      <Router>
+        <AddMovement />
+      </Router>
+    </ReduxProvider>
   )
   expect(asFragment()).toMatchSnapshot()
   expect(screen.getByText('Movement details')).toBeInTheDocument()
 });
 
 test('Should handle date selection', () => {
-   useAuth0.mockReturnValue({ user: {email: 'foo@bar.com' }, isAuthenticated: true, isLoading: false})
+  useAuth0.mockReturnValue({ user: {email: 'foo@bar.com' }, isAuthenticated: true, isLoading: false})
   const selectedDate = new Date(2019, 8, 15)
   const { asFragment} = render(
-    <Router>
-      <AddMovement userCoins={[]} dispatch={() => { }} selectedDate={selectedDate} />
-    </Router>
+    <ReduxProvider store={store}>
+      <Router>
+        <AddMovement selectedDate={selectedDate} />
+      </Router>
+    </ReduxProvider>
   )
   expect(asFragment()).toMatchSnapshot()
+  console.error.mockRestore();
 });
-
-
-
-  // console.error.mockRestore();
